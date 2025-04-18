17f8c8ffc364b6d440c6621e1b976ac1babb680a
larisa249 <larisarancov6@gmail.com>
2025-04-18 12:12:34 +0300

    virtual assistent

---
+++ src/main/java/com/lrs/SSB/controller/UserQuestion.java
@@ -0,0 +1,20 @@
+package com.lrs.SSB.controller;
+
+public class UserQuestion {
+    private String question;
+
+    public UserQuestion() {
+    }
+
+    public UserQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+}
